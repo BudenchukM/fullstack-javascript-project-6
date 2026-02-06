b310e00c5767a20d6532d4bfc61413976c64c453
BudenchukM <budenchuk.marina@gmail.com>
2026-02-06 14:30:53 +0300

    fix pug layout

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-// app.js
+const path = require('path');
 const fastify = require('fastify')({ logger: true });
 
-fastify.get('/', async (request, reply) => {
-  return 'Привет, Hexlet!';
+const view = require('@fastify/view');
+const fastifyStatic = require('@fastify/static');
+const i18next = require('i18next');
+const Backend = require('i18next-fs-backend');
+
+// i18n
+i18next.use(Backend).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  backend: {
+    loadPath: path.join(__dirname, 'locales/{{lng}}.json'),
+  },
+});
+
+fastify.decorate('t', i18next.t.bind(i18next));
+
+// static (bootstrap js/css later)
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/',
+});
+
+// pug
+fastify.register(view, {
+  engine: { pug: require('pug') },
+  root: path.join(__dirname, 'views'),
+});
+
+// routes
+fastify.get('/', async (req, reply) => {
+  return reply.view('index.pug', { t: fastify.t });
 });
 
 module.exports = fastify;
