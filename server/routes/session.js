81ba35d6ea785141c926a85e19ad703d96714b7d
BudenchukM <budenchuk.marina@gmail.com>
2026-02-25 14:33:22 +0300

    crud

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,3 +1,5 @@
+import i18next from 'i18next';
+
 export default (app) => {
   app.get('/session/new', (request, reply) => {
     const signInForm = {};
@@ -12,17 +14,17 @@ export default (app) => {
     }
     if (!user) {
       const signInForm = request.body.data;
-      request.flash('error', request.t('flash.session.create.error'));
+      request.flash('error', i18next.t('flash.session.create.error')); // ← i18next.t вместо request.t
       return reply.redirect('/session/new');
     }
     await request.logIn(user);
-    request.flash('success', request.t('flash.session.create.success'));
+    request.flash('success', i18next.t('flash.session.create.success')); // ← i18next.t вместо request.t
     return reply.redirect('/');
   }));
 
   app.delete('/session', async (request, reply) => {
     await request.logOut();
-    request.flash('info', request.t('flash.session.delete.success'));
+    request.flash('info', i18next.t('flash.session.delete.success')); // ← i18next.t вместо request.t
     return reply.redirect('/');
   });
 };
