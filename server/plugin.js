3906f0fbc4e49ab920d1453b8f54bc592f39b234
BudenchukM <budenchuk.marina@gmail.com>
2026-02-25 15:12:56 +0300

    crud

--- server/plugin.js
+++ server/plugin.js
@@ -39,7 +39,7 @@ const setUpViews = (app) => {
   });
 
   app.decorateReply('render', function render(viewPath, locals) {
-    this.view(viewPath, { ...locals, reply: this });
+    return this.view(viewPath, { ...locals, reply: this });
   });
 };
 
